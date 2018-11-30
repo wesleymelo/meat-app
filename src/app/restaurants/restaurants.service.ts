0cddf58f2d760c0aedb2656786146f050621a8a4
Wesley Melo <wesley.melo.pereira@gmail.com>
2018-11-30 21:29:07 -0200

    Seção 6 Aula 54

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/catch'
 import { Restaurant } from "./restaurant/restaurant.model";
 import { MEAT_API } from "app/app.api";
 import { ErrorHandler } from "app/app.error-handler";
+import { MenuItem } from "../restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
 export class RestaurantService{
@@ -20,6 +21,18 @@ export class RestaurantService{
 
     restaurantById(id: string): Observable<Restaurant>{
         return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    reviewasOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
